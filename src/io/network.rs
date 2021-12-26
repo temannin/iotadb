01519cb0e4f11dbb6d4171333a90d3130f98eba9
Tyler Manning <tylerericmanning@gmail.com>
2021-12-26 03:17:40 -0500

    Moving files around for better organization

---
+++ src/io/network.rs
@@ -0,0 +1,64 @@
+use std::io::{Error, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::str;
+use std::thread;
+
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser;
+
+pub fn start_network() {
+    let listener = TcpListener::bind("0.0.0.0:3306").unwrap();
+    // accept connections and process them, spawning a new thread for each one
+    info!("Server listening on port 3306");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                info!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move || {
+                    let result = handle_client(stream);
+                    match result {
+                        Ok(()) => {
+                            info!("Connection closed.")
+                        }
+                        Err(err) => {
+                            error!("Error: {}", err)
+                        }
+                    }
+                });
+            }
+            Err(e) => {
+                error!("Error: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
+    info!("Connection from {}", stream.peer_addr()?);
+
+    loop {
+        trace!("declaring buffer");
+        let mut buffer = [0; 4096];
+        let nbytes = stream.read(&mut buffer)?;
+
+        if nbytes == 0 {
+            trace!("nbytes was 0");
+            return Ok(());
+        }
+
+        let s = match str::from_utf8(&buffer[..nbytes]) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+
+        trace!("The client sent: {}", s);
+
+        let dialect = GenericDialect {};
+        let ast = Parser::parse_sql(&dialect, &s).unwrap();
+
+        println!("AST: {:?}", ast);
+
+        stream.write("Hello client!".as_bytes())?;
+        stream.flush()?;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
 use std::fs::File;
-use std::io::{Error, Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::str;
-use std::thread;
 
 use argparse::{ArgumentParser, Store, StoreTrue};
 
@@ -10,28 +6,6 @@ use argparse::{ArgumentParser, Store, StoreTrue};
 extern crate log;
 extern crate simplelog;
 use simplelog::*;
-
-fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
-    info!("Connection from {}", stream.peer_addr()?);
-    loop {
-        let mut buffer = [0; 4096];
-        let nbytes = stream.read(&mut buffer)?;
-
-        if nbytes == 0 {
-            return Ok(());
-        }
-
-        let s = match str::from_utf8(&buffer) {
-            Ok(v) => v,
-            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-        };
-
-        trace!("The client sent: {}", s);
-
-        stream.write("Hello client!".as_bytes())?;
-        stream.flush()?;
-    }
-}
 
 fn set_logger(log_path: String) {
     CombinedLogger::init(vec![
@@ -44,46 +18,20 @@ fn set_logger(log_path: String) {
         WriteLogger::new(
             LevelFilter::Info,
             Config::default(),
-            File::create(".log").unwrap(),
+            File::create(format!("{}.iota", log_path)).unwrap(),
         ),
     ])
     .unwrap();
 
     let mut ap = ArgumentParser::new();
     ap.set_description(
-        "iotadb. A completely open-source database that prioritizes safety over anything else. Learn more at: https://github.com/temannin/iotadb",
+        "Don't use this for anything remotely production worthy. I am learning how databases work by creating one so this will be slower, less-stable, and generally worse than any database out there ;).  Learn more at: https://github.com/temannin/iotadb.",
     );
     ap.parse_args_or_exit();
 }
 
-fn start_network() {
-    let listener = TcpListener::bind("0.0.0.0:3306").unwrap();
-    // accept connections and process them, spawning a new thread for each one
-    info!("Server listening on port 3306");
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                info!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move || {
-                    let result = handle_client(stream);
-                    match result {
-                        Ok(()) => {
-                            info!("Connection closed.")
-                        }
-                        Err(err) => {
-                            error!("Error: {}", err)
-                        }
-                    }
-                });
-            }
-            Err(e) => {
-                error!("Error: {}", e);
-            }
-        }
-    }
-}
-
+mod io;
 fn main() {
-    set_logger();
-    start_network();
+    set_logger("log".to_string());
+    io::network::start_network();
 }
